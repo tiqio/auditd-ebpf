1605530ae6d5705ba7d0f52e10031cc239e34d74
tiqio <tiqio@users.noreply.github.com>
2026-08-11 11:51:41 +0800

    test: isolate lifecycle rules from host auditd

--- tests/privileged/lifecycle_restart.sh
+++ tests/privileged/lifecycle_restart.sh
@@ -11,15 +11,23 @@ ebpf_object=${2:-}
 workdir=$(mktemp -d /tmp/auditd-ebpf-lifecycle.XXXXXX)
 chmod 0700 "${workdir}"
 state=${workdir}/lifecycle.toml
+rules_dir=${workdir}/rules.d
 first_out=${workdir}/first.out
 first_err=${workdir}/first.err
 second_out=${workdir}/second.out
 second_err=${workdir}/second.err
 trap 'rm -rf "${workdir}"' EXIT
+mkdir -m 0750 "${rules_dir}"
+cat >"${rules_dir}/10-lifecycle.rules" <<'EOF'
+-a always,exit -F arch=b64 -S execve -k lifecycle-test
+EOF
+chown root:root "${rules_dir}/10-lifecycle.rules"
+chmod 0640 "${rules_dir}/10-lifecycle.rules"
 run_args=(run)
 if [[ -n ${ebpf_object} ]]; then
   run_args+=(--ebpf-object "${ebpf_object}")
 fi
+run_args+=(--rules-dir "${rules_dir}")
 run_args+=(--lifecycle-state-file "${state}")
 
 "${binary}" "${run_args[@]}" >"${first_out}" 2>"${first_err}" &
